6dea9e1f9c1b2e7e1a0636b0c2b4ed5564995e09
ThaddeusGamit <thaddeusgamit31@gmail.com>
2023-12-27 18:39:22 +0800

    first commit

---
+++ src/app/client-details/client-detail-form/client-detail-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-client-detail-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './client-detail-form.component.html',
+  styles: ``
+})
+export class ClientDetailFormComponent {
+
+}
---
+++ src/app/client-details/client-details.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-client-details',
+  standalone: true,
+  imports: [],
+  templateUrl: './client-details.component.html',
+  styles: ``
+})
+export class ClientDetailsComponent {
+
+}
